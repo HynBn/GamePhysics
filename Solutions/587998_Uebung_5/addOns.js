9f515d2ff022eda591cde63101eec062bb0b735c
HynB <135057697+HynBn@users.noreply.github.com>
2024-11-12 17:38:59 +0100

    Add Uebung 5

---
+++ Solutions/587998_Uebung_5/addOns.js
@@ -0,0 +1,15 @@
+/* template GTAT2 Game Technology & Interactive Systems - addOns */
+/* Autor: Hyun Bin Jeoung 587998 */
+/* Übung Nr. 5*/
+/* Datum: 12.11.2024*/
+
+
+
+/* draw red rectangle */
+function drawRedRectangle(x, y, w, h, c)
+	{
+		fill(c);
+		rectMode(CORNER);
+		rect(x, y, w, h);
+	}
+
